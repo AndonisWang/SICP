cb82e979fc7a90170b46f6455ecf6661a39ec9fa
andonis <andoniswang@gmail.com>
2023-01-14 17:33:27 -0800

    SICP Code

---
+++ 1.2.1 Linear Recursion and Iteration Method 2 Recursive Procedure with Iterative Process.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+(define (factorial n)
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* product counter)
+                 (+ counter 1)
+                 max-count)))
+
+(factorial 6)
+
+#|
+; My own implementation
+(define (fact-iter n count)
+  (if (= count n)
+      n
+      (* count (fact-iter n (+ count 1)))))
+
+(define (factorial n)
+  (fact-iter n 1))
+
+(factorial 6)
+|#
